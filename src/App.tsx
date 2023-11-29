d4744e52f583e3de294648d9ffb033d03c5ee486
Gabriel <gszcode@gmail.com>
2023-11-29 01:13:18 -0300

    AGREGAR layout a la app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import SMNavbar from './components/SMNavbar'
+import { Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './page/Home'
 import Login from './page/Login'
+import Register from './page/Register'
 
 function App() {
   return (
-    <>
-      <header>
-        <SMNavbar />
-      </header>
-
-      <main>
-        <Login />
-      </main>
-    </>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Routes>
+    </Layout>
   )
 }
 
