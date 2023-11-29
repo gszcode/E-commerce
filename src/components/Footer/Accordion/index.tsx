b5912c06c2c4b3e4ad453de44aa521bf0d40c0f8
Gabriel <gszcode@gmail.com>
2023-11-29 01:14:33 -0300

    CREAR componente footer

---
+++ src/components/Footer/Accordion/index.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useRef, useState } from 'react'
+import styles from './accordion.module.scss'
+import { Link } from 'react-router-dom'
+
+interface Props {
+  title: string
+  links: Array<string>
+}
+
+function Accordion(props: Props) {
+  const [active, setActive] = useState(false)
+  const content = useRef<HTMLDivElement>(null)
+  const [height, setHeight] = useState('0px')
+
+  useEffect(() => {
+    console.log('Height for ', props.title, ': ', height)
+  }, [height, props.title])
+
+  function toggleAccordion() {
+    setActive(!active)
+    setHeight(active ? '0px' : `${content?.current?.scrollHeight}px`)
+  }
+
+  return (
+    <div className={styles.accordion__section}>
+      <div
+        className={`${styles.accordion} ${active ? styles.active : ''}`}
+        onClick={toggleAccordion}
+      >
+        <p className={styles.accordion__title}>{props.title}</p>
+        <span style={{ marginLeft: '20px' }}>{active ? '-' : '+'}</span>
+      </div>
+      <div
+        ref={content}
+        style={{ maxHeight: `${height}` }}
+        className={styles.accordion__content}
+      >
+        <div className={styles.accordion__text}>
+          {props.links.map((link) =>
+            props.title !== 'CONTACTO' ? (
+              <Link
+                to={`/${link.toLocaleLowerCase().split(' ').join('-')}`}
+                key={link}
+                className={styles.link}
+              >
+                {link}
+              </Link>
+            ) : (
+              <p className={styles.link}>{link}</p>
+            )
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Accordion
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,59 @@
+import Accordion from './Accordion'
+import styles from './footer.module.scss'
+import CheckBox from '../CheckBox'
+
+const Footer = () => {
+  return (
+    <footer className={styles.container}>
+      <div className={styles.offers_container}>
+        <h4 className={styles.span}>¡Recibe ofertas en tu correo!</h4>
+        <div className={styles.input_container}>
+          <input type="email" placeholder="Escribe tu e-mail" />
+          <img src="/icons/message.svg" alt="Message Icon" />
+        </div>
+        <CheckBox
+          text="He leído y estoy conforme con la"
+          linkText="Política de Protección de Datos"
+          href="/"
+        />
+      </div>
+      <div className={styles.links}>
+        <Accordion
+          title="INFORMACIÓN"
+          links={[
+            'Contacto',
+            'Envios y devoluciones',
+            'Condiciones generales',
+            'FAQ',
+            'MAPA web'
+          ]}
+        />
+        <Accordion
+          title="SOBRE NOSOTROS"
+          links={[
+            'Quienes somos',
+            'Contacto',
+            'Aviso Legal',
+            'Política y privacidad',
+            'Política de cookies'
+          ]}
+        />
+        <Accordion
+          title="CUENTA"
+          links={['Mi cuenta', 'Mis pedidos', 'Mis datos', 'Mis direcciones']}
+        />
+        <Accordion
+          title="CONTACTO"
+          links={[
+            'minimo-shoes.com',
+            'DIRECCIÓN: Avenida Alameda 74 03803 Alcoy (Alicante) España',
+            'TELÉFONO: 966523583',
+            'E-MAIL: test@mabisy.com'
+          ]}
+        />
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
