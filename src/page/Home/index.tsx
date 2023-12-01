5a90cb2462d9ba27bd61009dc9f731321958fc22
Gabriel <gszcode@gmail.com>
2023-12-01 01:08:47 -0300

    CREAR pagina home

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,5 +1,52 @@
+import HomeBanner from '../../components/HomeBanner'
+import styles from './home.module.scss'
+import banner from '/assets/home_banner.webp'
+import banner_01 from '/assets/home_section_banner_01.jpg'
+import banner_02 from '/assets/home_section_banner_02.jpg'
+import banner_03 from '/assets/home_section_banner_03.jpg'
+import banner_04 from '/assets/home_section_banner_04.jpg'
+import banner_05 from '/assets/home_section_banner_05.jpg'
+
 const Home = () => {
-  return <section>Home</section>
+  return (
+    <section className={styles.container}>
+      <div className={styles.banner}>
+        <img src={banner} alt="Banner Image" />
+      </div>
+      <div className={styles.sections}>
+        <HomeBanner
+          img={banner_01}
+          altText="Offers Banner"
+          title="OFERTAS INCREIBLES"
+          href="amazing-deals"
+        />
+        <HomeBanner
+          img={banner_02}
+          altText="Equipment Banner"
+          title="EQUIPAMIENTO"
+          href="equipment"
+        />
+        <HomeBanner
+          img={banner_03}
+          altText="Mountain Bikes Banner"
+          title="BICICLETAS MONTAÑERA"
+          href="mountain-bikes"
+        />
+        <HomeBanner
+          img={banner_04}
+          altText="Road Bikes Banner"
+          title="BICICLETAS CARRETERA"
+          href="road-bikes"
+        />
+        <HomeBanner
+          img={banner_05}
+          altText="Home Banner"
+          title="HOGAR"
+          href="home"
+        />
+      </div>
+    </section>
+  )
 }
 
 export default Home
