93799a4d9b59fd616407263b39d66c8c83c482af
Gabriel <gszcode@gmail.com>
2023-12-05 05:26:35 -0300

    CREAR carpeta para datos de prueba

---
+++ src/json/offers_products.ts
@@ -0,0 +1,38 @@
+export const products = [
+  {
+    id: 1,
+    image: '/assets/offers_01.webp',
+    title: 'Product 1',
+    rating: 4,
+    description: 'Description for Product 1',
+    price: 50,
+    prev_price: 60
+  },
+  {
+    id: 2,
+    image: '/assets/offers_02.webp',
+    title: 'Product 2',
+    rating: 3,
+    description: 'Description for Product 2',
+    price: 35,
+    prev_price: 45
+  },
+  {
+    id: 3,
+    image: '/assets/offers_03.jpg',
+    title: 'Product 3',
+    rating: 4,
+    description: 'Description for Product 3',
+    price: 50,
+    prev_price: 60
+  },
+  {
+    id: 4,
+    image: '/assets/offers_04.jpg',
+    title: 'Product 4',
+    rating: 3,
+    description: 'Description for Product 4',
+    price: 35,
+    prev_price: 45
+  }
+]
