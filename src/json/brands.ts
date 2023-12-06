b3332059e119e43d3c29e5cde6dfa11e479e0a2f
Gabriel <gszcode@gmail.com>
2023-12-06 03:21:42 -0300

    AGREGAR data marcas

---
+++ src/json/brands.ts
@@ -0,0 +1,32 @@
+export const brands = [
+  {
+    id: 1,
+    image: '/assets/brand_01.png',
+    title: 'Gigabyte'
+  },
+  {
+    id: 2,
+    image: '/assets/brand_02.png',
+    title: 'Kingston'
+  },
+  {
+    id: 3,
+    image: '/assets/brand_03.jpg',
+    title: 'Samsung'
+  },
+  {
+    id: 4,
+    image: '/assets/brand_04.png',
+    title: 'LG'
+  },
+  {
+    id: 5,
+    image: '/assets/brand_05.png',
+    title: 'Adidas'
+  },
+  {
+    id: 6,
+    image: '/assets/brand_06.png',
+    title: 'Nike'
+  }
+]
