deab4686053dc97a41525050fa761d1eb04ec046
Gabriel <gszcode@gmail.com>
2023-12-07 01:34:22 -0300

    AGREGAR datos de servicios

---
+++ src/json/services.ts
@@ -0,0 +1,31 @@
+export const services = [
+  {
+    id: 1,
+    icon: '/icons/trunk.svg',
+    title: 'ENTREGA URGENTE',
+    description: 'Nuestro equipo de transporte siempre preparado',
+    link: 'shipping-&-returns'
+  },
+  {
+    id: 2,
+    icon: '/icons/phone.svg',
+    title: 'SOPORTE TELEFÓNICO',
+    description:
+      'Contacta con nosotros de Lunes a Viernes, de 10:00hs a 20:00hs',
+    link: 'telephone-support'
+  },
+  {
+    id: 3,
+    icon: '/icons/users.svg',
+    title: 'PERSONAL CUALIFICADO',
+    description: 'Nuestros operarios tienen años de experiencia',
+    link: ''
+  },
+  {
+    id: 4,
+    icon: '/icons/bag.svg',
+    title: 'FACILIDAD DE PAGO',
+    description: 'Facilitamos tus encargos',
+    link: ''
+  }
+]
