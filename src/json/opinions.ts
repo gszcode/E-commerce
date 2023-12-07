56ec5ddc19bd279a6c17bf14be281f7910681580
Gabriel <gszcode@gmail.com>
2023-12-07 00:33:44 -0300

    AGREGAR datos de opiniones

---
+++ src/json/opinions.ts
@@ -0,0 +1,30 @@
+export const opinions = [
+  {
+    id: 1,
+    user: 'Demo',
+    opinion:
+      'In sed orci rhoncus, pharetra libero sit amet, cursus diam. Proin mattis massa id pharetra placerat. Quisque nisi nisi, consequat quis purus a, lobortis pulvinar enim. Aliquam dapibus felis eget.',
+    date: '16/04/2020 00:14'
+  },
+  {
+    id: 2,
+    user: 'Demo',
+    opinion:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec feugiat magna elit, vel hendrerit nulla maximus id. Morbi consectetur ante maximus, feugiat nunc sit amet, tristique lacus. Fusce suscipit orci eros. In dictum sit amet ante non laoreet. Nunc ultrices ante mi, ut euismod orci ullamcorper non. In semper quam ut augue pharetra maximus. Aliquam erat volutpat.',
+    date: '16/04/2020 00:14'
+  },
+  {
+    id: 3,
+    user: 'Demo',
+    opinion:
+      'In sed orci rhoncus, pharetra libero sit amet, cursus diam. Proin mattis massa id pharetra placerat. Quisque nisi nisi, consequat quis purus a, lobortis pulvinar enim. Aliquam dapibus felis eget.',
+    date: '16/04/2020 00:14'
+  },
+  {
+    id: 4,
+    user: 'Demo',
+    opinion:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec feugiat magna elit, vel hendrerit nulla maximus id. Morbi consectetur ante maximus, feugiat nunc sit amet, tristique lacus. Fusce suscipit orci eros. In dictum sit amet ante non laoreet. Nunc ultrices ante mi, ut euismod orci ullamcorper non. In semper quam ut augue pharetra maximus. Aliquam erat volutpat.',
+    date: '16/04/2020 00:14'
+  }
+]
