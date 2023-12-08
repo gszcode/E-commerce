6d8dd3dcbf0934b2081ede08145535873dc99ac7
Gabriel <gszcode@gmail.com>
2023-12-08 18:45:10 -0300

    CREAR componente menu

---
+++ src/components/Image/index.tsx
@@ -0,0 +1,20 @@
+import styles from './image.module.scss'
+
+interface ImageProps {
+  img: string
+  alt: string
+  title: string
+}
+
+const Image = ({ img, alt, title }: ImageProps) => {
+  return (
+    <img
+      className={styles.icon}
+      src={`/icons/${img}.svg`}
+      alt={alt}
+      title={title}
+    />
+  )
+}
+
+export default Image
