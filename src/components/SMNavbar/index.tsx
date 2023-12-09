78763ac31de976b7eeb88e23ad7a58ddde70409e
Gabriel <gszcode@gmail.com>
2023-12-09 05:12:40 -0300

    AGREGAR user menu

--- src/components/SMNavbar/index.tsx
+++ src/components/SMNavbar/index.tsx
@@ -4,18 +4,30 @@ import LGNavbar from '../LGNavbar'
 import Menu from './Menu'
 import { useState } from 'react'
 import Image from '../Image'
+import { user_menu_links } from '../../json/links'
+import { SubMenu } from '../../typescript/types/submenu'
+import SubNav from '../LGNavbar/SubNav'
 
 const SMNavbar = () => {
   const [openMenu, setOpenMenu] = useState(false)
+  const [openUserMenu, setOpenUserMenu] = useState<SubMenu>(null)
 
   const handleOpenMenu = () => {
     setOpenMenu((prev) => !prev)
   }
 
+  const handleOpenSubNav = (menu: SubMenu) => {
+    setOpenUserMenu(() => (openUserMenu === menu ? null : menu))
+  }
+
   return (
     <nav className={styles.container}>
       <div className={styles.title_container}>
-        <button onClick={handleOpenMenu} className={styles.bars} title="Menú">
+        <button
+          onClick={handleOpenMenu}
+          className={`icon_btn ${styles.bars}`}
+          title="Menú"
+        >
           <Image img="bars" alt="Bars" title="Menú" />
         </button>
         <Link to="/" className={styles.title} title="Título">
@@ -32,7 +44,17 @@ const SMNavbar = () => {
       <div className={styles.icons}>
         <Image img="search" alt="Search" title="Buscar" />
         <Image img="cart" alt="Cart" title="Carrito" />
-        <Image img="user" alt="User" title="Mi cuenta" />
+        <button onClick={() => handleOpenSubNav('user')} className="icon_btn">
+          <Image img="user" alt="User" title="Mi cuenta" />
+
+          {openUserMenu && (
+            <SubNav
+              links={user_menu_links}
+              openSubNav={openUserMenu}
+              setOpenSubNav={setOpenUserMenu}
+            />
+          )}
+        </button>
       </div>
     </nav>
   )
