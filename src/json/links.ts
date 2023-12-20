f7fee68ff4c6d082aad5a1cd483286cc405cd68b
Gabriel <gszcode@gmail.com>
2023-12-20 02:52:01 -0300

    agregar links de user logueado

--- src/json/links.ts
+++ src/json/links.ts
@@ -40,3 +40,26 @@ export const user_menu_links = [
     icon: 'user'
   }
 ]
+
+export const active_user_menu_links = [
+  {
+    link: 'Mi cuenta',
+    href: 'account',
+    icon: 'user'
+  },
+  {
+    link: 'Mis pedidos',
+    href: 'orders',
+    icon: 'cart'
+  },
+  {
+    link: 'Mis favoritos',
+    href: 'favorite-products',
+    icon: 'heart'
+  },
+  {
+    link: 'Cerrar sesión',
+    href: 'logput',
+    icon: 'logout'
+  }
+]
