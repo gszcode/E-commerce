f32cffe47765266121aefc829378649b245ae4ab
Gabriel <gszcode@gmail.com>
2023-12-19 21:31:32 -0300

    wrapper agregado

--- src/page/Account/index.tsx
+++ src/page/Account/index.tsx
@@ -1,5 +1,7 @@
+import AccountTitle from '../../components/AccountTitle'
 import BreadCrumbs from '../../components/BreadCrumbs'
 import Sidebar from '../../components/Sidebar'
+import Wrapper from '../../components/Wrapper'
 import styles from './account.module.scss'
 import { Link } from 'react-router-dom'
 
@@ -7,36 +9,38 @@ const Account = () => {
   return (
     <section className={styles.container}>
       <BreadCrumbs page="Mi cuenta" />
-      <div className={styles.wrapper}>
-        <Sidebar />
-        <div className={styles.account}>
-          <h1 className={styles.title}>MI CUENTA</h1>
-          <div className={styles.data_table}>
-            <h3 className={styles.subtitle}>MIS DATOS DE CLIENTE</h3>
-            <div className={styles.table}>
-              <p className={styles.item}>
-                <strong>Username: </strong>
-                <span>gaby</span>
-              </p>
-              <p className={styles.item}>
-                <strong>Nombre: </strong>
-                <span>gabriel</span>
-              </p>
-              <p className={styles.item}>
-                <strong>Apellido: </strong>
-                <span>sanchez</span>
-              </p>
-              <p className={styles.item}>
-                <strong>Email: </strong>
-                <span>sgabriel.dev@gmail.com</span>
-              </p>
+      <Wrapper>
+        <>
+          <Sidebar />
+          <div className={styles.account}>
+            <AccountTitle title="MI CUENTA" />
+            <div className={styles.data_table}>
+              <h3 className={styles.subtitle}>MIS DATOS DE CLIENTE</h3>
+              <div className={styles.table}>
+                <p className={styles.item}>
+                  <strong>Username: </strong>
+                  <span>gaby</span>
+                </p>
+                <p className={styles.item}>
+                  <strong>Nombre: </strong>
+                  <span>gabriel</span>
+                </p>
+                <p className={styles.item}>
+                  <strong>Apellido: </strong>
+                  <span>sanchez</span>
+                </p>
+                <p className={styles.item}>
+                  <strong>Email: </strong>
+                  <span>sgabriel.dev@gmail.com</span>
+                </p>
+              </div>
             </div>
+            <Link className={styles.btn} to="/account/edit-data">
+              MODIFICAR MIS DATOS
+            </Link>
           </div>
-          <Link className={styles.btn} to="/edit-data">
-            MODIFICAR MIS DATOS
-          </Link>
-        </div>
-      </div>
+        </>
+      </Wrapper>
     </section>
   )
 }
