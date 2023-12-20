55625751f4128096f856b9d51a43e077536fc8c3
Gabriel <gszcode@gmail.com>
2023-12-19 22:24:31 -0300

    crear componente de dataitem

---
+++ src/page/Account/DataItem/index.tsx
@@ -0,0 +1,17 @@
+import styles from './data_item.module.scss'
+
+interface ItemProps {
+  label: string
+  data: string
+}
+
+const DataItem = ({ label, data }: ItemProps) => {
+  return (
+    <p className={styles.item}>
+      <strong>{label}: </strong>
+      <span>{data}</span>
+    </p>
+  )
+}
+
+export default DataItem
