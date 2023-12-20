9482c0f87397b60e76089077c9589fdc8486f79f
Gabriel <gszcode@gmail.com>
2023-12-19 22:25:17 -0300

    crear componente para items

--- src/page/Account/index.tsx
+++ src/page/Account/index.tsx
@@ -2,6 +2,7 @@ import AccountTitle from '../../components/AccountTitle'
 import BreadCrumbs from '../../components/BreadCrumbs'
 import Sidebar from '../../components/Sidebar'
 import Wrapper from '../../components/Wrapper'
+import DataItem from './DataItem'
 import styles from './account.module.scss'
 import { Link } from 'react-router-dom'
 
@@ -17,22 +18,10 @@ const Account = () => {
             <div className={styles.data_table}>
               <h3 className={styles.subtitle}>MIS DATOS DE CLIENTE</h3>
               <div className={styles.table}>
-                <p className={styles.item}>
-                  <strong>Username: </strong>
-                  <span>gaby</span>
-                </p>
-                <p className={styles.item}>
-                  <strong>Nombre: </strong>
-                  <span>gabriel</span>
-                </p>
-                <p className={styles.item}>
-                  <strong>Apellido: </strong>
-                  <span>sanchez</span>
-                </p>
-                <p className={styles.item}>
-                  <strong>Email: </strong>
-                  <span>sgabriel.dev@gmail.com</span>
-                </p>
+                <DataItem label="Username" data="gaby" />
+                <DataItem label="Nombre" data="gabriel" />
+                <DataItem label="Apellido" data="sanchez" />
+                <DataItem label="Email" data="sgabrielok01@gmail.com" />
               </div>
             </div>
             <Link className={styles.btn} to="/account/edit-data">
