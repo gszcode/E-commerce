7ea7d4b1ab7691ee4e51a0313bab1eb7e0eb9b85
Gabriel <gszcode@gmail.com>
2023-12-20 22:53:16 -0300

    agregar estilos a svg de mostrar password

--- src/components/InputLoginRegister/index.tsx
+++ src/components/InputLoginRegister/index.tsx
@@ -1,16 +1,49 @@
+import { FormikProps } from 'formik'
 import styles from './login_register.module.scss'
+import Image from '../Image'
+import { useState } from 'react'
 
 interface PropsInput {
   labelText: string
   textType: string
   textPlaceholder: string
+  name: string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  formik: FormikProps<any>
 }
 
-const Input = ({ labelText, textType, textPlaceholder }: PropsInput) => {
+const Input = ({
+  labelText,
+  textType,
+  textPlaceholder,
+  formik,
+  name
+}: PropsInput) => {
+  const [showPass, setShowPass] = useState(false)
+
+  const showPassword = () => {
+    setShowPass(!showPass)
+  }
+
   return (
     <div className={styles.input_container}>
       <label>{labelText}</label>
-      <input type={textType} placeholder={textPlaceholder} />
+      <input
+        type={!showPass ? textType : 'text'}
+        placeholder={textPlaceholder}
+        onChange={formik.handleChange}
+        name={name}
+        value={formik.values[name].toString()}
+      />
+      {name === 'password' && (
+        <button
+          type="button"
+          className={`icon_btn ${styles.eye}`}
+          onClick={showPassword}
+        >
+          <Image img={!showPass ? 'eye' : 'eye_slash'} alt="Eye" />
+        </button>
+      )}
     </div>
   )
 }
