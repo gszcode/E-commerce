9a62f97e2bc51be219d5b8008f0e54a03318f936
Gabriel <gszcode@gmail.com>
2023-12-21 00:41:14 -0300

    modificacion de type user

--- src/typescript/interfaces/user.interface.ts
+++ src/typescript/interfaces/user.interface.ts
@@ -1,11 +1,16 @@
 interface User {
-  first_name: string
-  last_name: string
-  username: string
+  first_name?: string
+  last_name?: string
+  username?: string
   email: string
 }
 
 export interface UserSlice {
   message: string
   data?: User | null
+}
+
+export interface Auth extends User {
+  password: string
+  [key: string]: string | undefined
 }
