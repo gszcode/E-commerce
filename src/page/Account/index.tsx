c93e267ff0e984a1f5a69eb934ff94600cb806be
Gabriel <gszcode@gmail.com>
2023-12-22 20:52:35 -0300

    hacer peticion de dator del usuario a la api

--- src/page/Account/index.tsx
+++ src/page/Account/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import AccountTitle from '../../components/AccountTitle'
 import BreadCrumbs from '../../components/BreadCrumbs'
 import Sidebar from '../../components/Sidebar'
@@ -5,8 +6,29 @@ import Wrapper from '../../components/Wrapper'
 import DataItem from './DataItem'
 import styles from './account.module.scss'
 import { Link } from 'react-router-dom'
+import { get } from '../../services/fetch'
+import { useDispatch, useSelector } from 'react-redux'
+import { UserApi } from '../../typescript/interfaces/user.interface'
+import { setUserData } from '../../store/features/user/userSlice'
 
 const Account = () => {
+  const dispatch = useDispatch()
+  const { user } = useSelector((state: UserApi) => state.user as UserApi)
+
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const response = await get('user/account')
+        dispatch(setUserData(response.data.data))
+        console.log(response)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    getUserData()
+  }, [dispatch])
+
   return (
     <section className={styles.container}>
       <BreadCrumbs page="Mi cuenta" />
@@ -18,10 +40,10 @@ const Account = () => {
             <div className={styles.data_table}>
               <h3 className={styles.subtitle}>MIS DATOS DE CLIENTE</h3>
               <div className={styles.table}>
-                <DataItem label="Username" data="gaby" />
-                <DataItem label="Nombre" data="gabriel" />
-                <DataItem label="Apellido" data="sanchez" />
-                <DataItem label="Email" data="sgabrielok01@gmail.com" />
+                <DataItem label="Username" data={user?.username} />
+                <DataItem label="Nombre" data={user?.first_name} />
+                <DataItem label="Apellido" data={user?.last_name} />
+                <DataItem label="Email" data={user?.email} />
               </div>
             </div>
             <Link className={styles.btn} to="/account/edit-data">
