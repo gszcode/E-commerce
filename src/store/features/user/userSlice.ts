7847508f880a83a057b51879ad87e05b2541f6bf
Gabriel <gszcode@gmail.com>
2023-12-23 02:37:15 -0300

    se agrego propiedad de isAuthenticated del user

--- src/store/features/user/userSlice.ts
+++ src/store/features/user/userSlice.ts
@@ -1,20 +1,23 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
-import { User, UserApi } from '../../../typescript/interfaces/user.interface'
+import { UserApi } from '../../../typescript/interfaces/user.interface'
 
 const initialState: UserApi = {
-  user: null
+  user: null,
+  isAuthenticated: false
 }
 
 export const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    setUserData: (state, action: PayloadAction<User>) => {
-      state.user = action.payload
+    setUserData: (state, action: PayloadAction<UserApi>) => {
+      state.user = action.payload.user
+      state.isAuthenticated = action.payload.isAuthenticated
     },
     setClearUser: (state) => {
       state.user = null
+      state.isAuthenticated = false
     }
   }
 })
