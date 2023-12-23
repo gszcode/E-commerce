0ec027c784765d20c5b293f008721d5f3bfcc6ae
Gabriel <gszcode@gmail.com>
2023-12-23 02:37:59 -0300

    se agrego validaciones del token

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './page/Home'
 import Login from './page/Login'
@@ -8,37 +8,79 @@ import Account from './page/Account'
 import EditData from './page/EditData'
 import Orders from './page/Orders'
 import FavoriteProducts from './page/FavoriteProducts'
-import { useSelector } from 'react-redux'
-import { UserApi } from './typescript/interfaces/user.interface'
 import axios from 'axios'
+import ProtectedRoute from './ProtectedRoute'
+import { useEffect, useState } from 'react'
+import Cookies from 'js-cookie'
+import { get } from './services/fetch'
+import { useDispatch } from 'react-redux'
+import { setUserData } from './store/features/user/userSlice'
 axios.defaults.withCredentials = true
 
 function App() {
-  const { user } = useSelector((state: UserApi) => state.user as UserApi)
+  const dispatch = useDispatch()
+  const [loading, setLoading] = useState(true)
+  console.log(loading)
+
+  useEffect(() => {
+    const checkLogin = async () => {
+      setLoading(true)
+      const cookies = Cookies.get()
+
+      if (!cookies.token) {
+        dispatch(setUserData({ user: null, isAuthenticated: false }))
+        setLoading(false)
+        return
+      }
+
+      try {
+        const response = await get('auth/verify-token')
+        if (response.status !== 200) {
+          dispatch(setUserData({ user: null, isAuthenticated: false }))
+          return
+        }
+
+        dispatch(setUserData({ user: response.data, isAuthenticated: true }))
+      } catch (error) {
+        dispatch(setUserData({ user: null, isAuthenticated: false }))
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    checkLogin()
+  }, [dispatch])
 
   return (
     <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
+      {loading ? (
+        <div
+          style={{
+            height: '70vh',
+            color: 'black',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          <p>Cargando...</p>
+        </div>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
 
-        {user !== null && user !== undefined ? (
-          <>
-            <Route path="account" element={<Account />} />
-            <Route path="account/edit-data" element={<EditData />} />
-            <Route path="orders" element={<Orders />} />
-            <Route path="favorite-products" element={<FavoriteProducts />} />
-            <Route path="*" element={<Navigate to="/account" />} />
-          </>
-        ) : (
-          <>
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
-            <Route path="*" element={<Navigate to="/login" />} />
-          </>
-        )}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/account" element={<Account />} />
+            <Route path="/account/edit-data" element={<EditData />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/favorite-products" element={<FavoriteProducts />} />
+          </Route>
 
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      )}
     </Layout>
   )
 }
