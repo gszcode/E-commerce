a2b05a75bd3cb67eb3f5a2c2ebb2a65e7bc8d495
Gabriel <gszcode@gmail.com>
2023-12-27 03:42:50 -0300

    se creo ruta de contacto

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import Loader from './components/Loader'
 import { useCheckLogin } from './hooks/useCheckLogin'
+import Contact from './page/Contact'
 axios.defaults.withCredentials = true
 
 function App() {
@@ -35,6 +36,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/contact" element={<Contact />} />
 
           <Route element={<ProtectedRoute />}>
             <Route path="/account" element={<Account />} />
