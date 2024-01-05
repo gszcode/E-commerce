ad2015c479be92ae63f1d44f4d7812cb9fb2015d
Gabriel <gszcode@gmail.com>
2024-01-05 02:18:37 -0300

    se crearon productos provicionales de home

---
+++ src/json/home_products.ts
@@ -0,0 +1,176 @@
+export const blankets = [
+  {
+    id: 1,
+    image: '/assets/blanket_01.jpg',
+    title: 'Product 1',
+    rating: 4,
+    description: 'Description for Product 1',
+    price: 50
+  },
+  {
+    id: 2,
+    image: '/assets/blanket_02.jpg',
+    title: 'Product 2',
+    rating: 3,
+    description: 'Description for Product 2',
+    price: 35
+  },
+  {
+    id: 3,
+    image: '/assets/blanket_03.jpg',
+    title: 'Product 3',
+    rating: 4,
+    description: 'Description for Product 3',
+    price: 50
+  },
+  {
+    id: 4,
+    image: '/assets/blanket_04.jpg',
+    title: 'Product 4',
+    rating: 3,
+    description: 'Description for Product 4',
+    price: 35
+  }
+]
+
+export const sofas = [
+  {
+    id: 1,
+    image: '/assets/sofa_01.jpg',
+    title: 'Product 1',
+    rating: 4,
+    description: 'Description for Product 1',
+    price: 50
+  },
+  {
+    id: 2,
+    image: '/assets/sofa_02.jpg',
+    title: 'Product 2',
+    rating: 3,
+    description: 'Description for Product 2',
+    price: 35
+  },
+  {
+    id: 3,
+    image: '/assets/sofa_03.jpg',
+    title: 'Product 3',
+    rating: 4,
+    description: 'Description for Product 3',
+    price: 50
+  },
+  {
+    id: 4,
+    image: '/assets/sofa_04.jpg',
+    title: 'Product 4',
+    rating: 3,
+    description: 'Description for Product 4',
+    price: 35
+  },
+  {
+    id: 5,
+    image: '/assets/sofa_05.jpg',
+    title: 'Product 5',
+    rating: 3,
+    description: 'Description for Product 5',
+    price: 35
+  },
+  {
+    id: 6,
+    image: '/assets/sofa_06.jpg',
+    title: 'Product 3',
+    rating: 4,
+    description: 'Description for Product 6',
+    price: 50
+  },
+  {
+    id: 7,
+    image: '/assets/sofa_07.jpg',
+    title: 'Product 7',
+    rating: 3,
+    description: 'Description for Product 7',
+    price: 35
+  }
+]
+
+export const several = [
+  {
+    id: 1,
+    image: '/assets/several_01.jpg',
+    title: 'Product 1',
+    rating: 4,
+    description: 'Description for Product 1',
+    price: 50
+  },
+  {
+    id: 2,
+    image: '/assets/several_02.jpg',
+    title: 'Product 2',
+    rating: 3,
+    description: 'Description for Product 2',
+    price: 35
+  },
+  {
+    id: 3,
+    image: '/assets/several_03.jpg',
+    title: 'Product 3',
+    rating: 4,
+    description: 'Description for Product 3',
+    price: 50
+  },
+  {
+    id: 4,
+    image: '/assets/several_04.jpg',
+    title: 'Product 4',
+    rating: 3,
+    description: 'Description for Product 4',
+    price: 35
+  },
+  {
+    id: 5,
+    image: '/assets/several_05.jpg',
+    title: 'Product 5',
+    rating: 3,
+    description: 'Description for Product 5',
+    price: 35
+  },
+  {
+    id: 6,
+    image: '/assets/several_06.jpg',
+    title: 'Product 3',
+    rating: 4,
+    description: 'Description for Product 6',
+    price: 50
+  },
+  {
+    id: 7,
+    image: '/assets/several_07.jpg',
+    title: 'Product 7',
+    rating: 3,
+    description: 'Description for Product 7',
+    price: 35
+  },
+  {
+    id: 8,
+    image: '/assets/several_08.jpg',
+    title: 'Product 8',
+    rating: 3,
+    description: 'Description for Product 8',
+    price: 35
+  },
+  {
+    id: 9,
+    image: '/assets/several_09.jpg',
+    title: 'Product 9',
+    rating: 4,
+    description: 'Description for Product 9',
+    price: 50
+  },
+  {
+    id: 10,
+    image: '/assets/several_10.jpg',
+    title: 'Product 10',
+    rating: 3,
+    description: 'Description for Product 10',
+    price: 35
+  }
+]
