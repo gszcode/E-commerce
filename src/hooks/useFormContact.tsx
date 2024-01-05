c083dcc0afe595ca0ee26952c713bb130e76a14b
Gabriel <gszcode@gmail.com>
2024-01-04 21:15:48 -0300

    controlador de envio de email

--- src/hooks/useFormContact.tsx
+++ src/hooks/useFormContact.tsx
@@ -1,7 +1,11 @@
 import { useFormik } from 'formik'
 import { Contact } from '../typescript/interfaces/contact.interface'
+import { post } from '../services/fetch'
+import { messageToast } from '../utils/toastSuccess'
 
 export const useFormContact = (initialValues: Contact) => {
+  const { notify, notifyError } = messageToast()
+
   const validate = (values: Contact) => {
     const errors: Partial<Contact> = {}
     const validateEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
@@ -18,7 +22,14 @@ export const useFormContact = (initialValues: Contact) => {
     initialValues,
     validate,
     onSubmit: async (values) => {
-      console.log(values)
+      const response = await post('contact', values)
+
+      if (response.status === 200) {
+        notify(response.data.message)
+        formik.resetForm()
+      } else {
+        notifyError(response.data.error || response.data.errors)
+      }
     }
   })
 
