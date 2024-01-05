18b34ed7a2e7056bc327bd5bec36720e726fdda7
Gabriel <gszcode@gmail.com>
2024-01-05 02:18:14 -0300

    se creo componentes de todos productos

---
+++ src/page/Products/AllProducts/index.tsx
@@ -0,0 +1,25 @@
+import Products from '..'
+import { products } from '../../../json/offers_products'
+import { productsPage } from '../../../json/products'
+import { blankets, several, sofas } from '../../../json/home_products'
+import {
+  pajamas_blankets,
+  sneakers,
+  underwear
+} from '../../../json/clothes_products'
+const allProducts = [
+  ...products,
+  ...pajamas_blankets,
+  ...sofas,
+  ...blankets,
+  ...underwear,
+  ...sneakers,
+  ...several,
+  ...productsPage
+]
+
+const AllProducts = () => {
+  return <Products products={allProducts} />
+}
+
+export default AllProducts
