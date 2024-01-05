85f302911274b3d883879a24563a8d43731738ab
Gabriel <gszcode@gmail.com>
2024-01-05 02:18:47 -0300

    se crearon productos provicionales de clothes

---
+++ src/json/clothes_products.ts
@@ -0,0 +1,152 @@
+export const pajamas_blankets = [
+  {
+    id: 1,
+    image: '/assets/pajama_blanket_01.jpg',
+    title: 'Product 1',
+    rating: 4,
+    description: 'Description for Product 1',
+    price: 50
+  },
+  {
+    id: 2,
+    image: '/assets/pajama_blanket_02.jpg',
+    title: 'Product 2',
+    rating: 3,
+    description: 'Description for Product 2',
+    price: 35
+  },
+  {
+    id: 3,
+    image: '/assets/pajama_blanket_03.jpg',
+    title: 'Product 3',
+    rating: 4,
+    description: 'Description for Product 3',
+    price: 50
+  },
+  {
+    id: 4,
+    image: '/assets/pajama_blanket_04.jpg',
+    title: 'Product 4',
+    rating: 3,
+    description: 'Description for Product 4',
+    price: 35
+  }
+]
+
+export const underwear = [
+  {
+    id: 1,
+    image: '/assets/underwear_01.jpg',
+    title: 'Product 1',
+    rating: 4,
+    description: 'Description for Product 1',
+    price: 50
+  },
+  {
+    id: 2,
+    image: '/assets/underwear_02.jpg',
+    title: 'Product 2',
+    rating: 3,
+    description: 'Description for Product 2',
+    price: 35
+  },
+  {
+    id: 3,
+    image: '/assets/underwear_03.jpg',
+    title: 'Product 3',
+    rating: 4,
+    description: 'Description for Product 3',
+    price: 50
+  },
+  {
+    id: 4,
+    image: '/assets/underwear_04.jpg',
+    title: 'Product 4',
+    rating: 3,
+    description: 'Description for Product 4',
+    price: 35
+  },
+  {
+    id: 6,
+    image: '/assets/underwear_06.jpg',
+    title: 'Product 6',
+    rating: 3,
+    description: 'Description for Product 6',
+    price: 35
+  },
+  {
+    id: 7,
+    image: '/assets/underwear_07.jpg',
+    title: 'Product 7',
+    rating: 3,
+    description: 'Description for Product 7',
+    price: 35
+  },
+  {
+    id: 8,
+    image: '/assets/underwear_08.jpg',
+    title: 'Product 8',
+    rating: 3,
+    description: 'Description for Product 8',
+    price: 35
+  },
+  {
+    id: 9,
+    image: '/assets/underwear_09.jpg',
+    title: 'Product 9',
+    rating: 3,
+    description: 'Description for Product 9',
+    price: 35
+  },
+  {
+    id: 10,
+    image: '/assets/underwear_10.jpg',
+    title: 'Product 10',
+    rating: 3,
+    description: 'Description for Product 10',
+    price: 35
+  }
+]
+
+export const sneakers = [
+  {
+    id: 1,
+    image: '/assets/sneaker_01.jpg',
+    title: 'Product 1',
+    rating: 4,
+    description: 'Description for Product 1',
+    price: 50
+  },
+  {
+    id: 2,
+    image: '/assets/sneaker_02.jpg',
+    title: 'Product 2',
+    rating: 3,
+    description: 'Description for Product 2',
+    price: 35
+  },
+  {
+    id: 3,
+    image: '/assets/sneaker_03.jpg',
+    title: 'Product 3',
+    rating: 4,
+    description: 'Description for Product 3',
+    price: 50
+  },
+  {
+    id: 4,
+    image: '/assets/sneaker_04.jpg',
+    title: 'Product 4',
+    rating: 3,
+    description: 'Description for Product 4',
+    price: 35
+  },
+  {
+    id: 5,
+    image: '/assets/sneaker_05.jpg',
+    title: 'Product 5',
+    rating: 3,
+    description: 'Description for Product 5',
+    price: 35
+  }
+]
