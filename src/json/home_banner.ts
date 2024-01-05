d32b8bf9792c6e59446c6f3a9048545a9ab2898d
Gabriel <gszcode@gmail.com>
2024-01-05 02:19:56 -0300

    se modificaron los links

--- src/json/home_banner.ts
+++ src/json/home_banner.ts
@@ -4,34 +4,34 @@ export const banners = [
     image: '/assets/home_section_banner_01.jpg',
     altText: 'Offers Banner',
     title: 'OFERTAS INCREIBLES',
-    href: 'amazing-deals'
+    href: 'products/amazing-deals'
   },
   {
     id: 2,
     image: '/assets/home_section_banner_02.jpg',
     altText: 'Equipment Banner',
     title: 'EQUIPAMIENTO',
-    href: 'equipment'
+    href: 'products/equipment'
   },
   {
     id: 3,
     image: '/assets/home_section_banner_03.jpg',
     altText: 'Mountain Bikes Banner',
     title: 'BICICLETAS MONTAÑERA',
-    href: 'mountain-bikes'
+    href: 'products/mountain-bikes'
   },
   {
     id: 4,
     image: '/assets/home_section_banner_04.jpg',
     altText: 'Road Bikes Banner',
     title: 'BICICLETAS CARRETERA',
-    href: 'road-bikes'
+    href: 'products/road-bikes'
   },
   {
     id: 5,
     image: '/assets/home_section_banner_05.jpg',
     altText: 'Home Banner',
     title: 'HOGAR',
-    href: 'home'
+    href: 'products/home'
   }
 ]
