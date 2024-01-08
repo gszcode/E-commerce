4e064d07fc314b8e0b5e51bcee2455ea99263cbd
Gabriel <gszcode@gmail.com>
2024-01-08 08:49:55 -0300

    se agrego el fetch put

--- src/services/fetch.ts
+++ src/services/fetch.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosError, AxiosResponse } from 'axios'
-import { Post } from '../typescript/types/post'
+import { Post, Put } from '../typescript/types/fetch'
 const api = 'http://localhost:3000/api/v1'
 
 const post = async (url: string, data?: Post): Promise<AxiosResponse> => {
@@ -26,4 +26,15 @@ const get = async (url: string): Promise<AxiosResponse> => {
   }
 }
 
-export { post, get }
+const put = async (url: string, data?: Put): Promise<AxiosResponse> => {
+  try {
+    const response = await axios.put(`${api}/${url}`, data)
+    return response
+  } catch (error) {
+    const axiosError = error as AxiosError
+    if (axiosError.response) return axiosError.response
+    else throw error
+  }
+}
+
+export { post, get, put }
