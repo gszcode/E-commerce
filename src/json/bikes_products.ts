eb627c89287498c80bca06df9ac98773bd06c68d
Gabriel <gszcode@gmail.com>
2024-01-08 23:32:50 -0300

    se agregaron los productos provicionales de sports, complements y bicicletas

---
+++ src/json/bikes_products.ts
@@ -0,0 +1,66 @@
+export const bikes = [
+  {
+    id: 1,
+    image: '/assets/bike_01.jpg',
+    title: 'Product 1',
+    rating: 4,
+    description: 'Description for Product 1',
+    price: 50
+  },
+  {
+    id: 2,
+    image: '/assets/bike_02.jpg',
+    title: 'Product 2',
+    rating: 3,
+    description: 'Description for Product 2',
+    price: 35
+  },
+  {
+    id: 3,
+    image: '/assets/bike_03.jpg',
+    title: 'Product 3',
+    rating: 4,
+    description: 'Description for Product 3',
+    price: 50
+  },
+  {
+    id: 4,
+    image: '/assets/bike_04.jpg',
+    title: 'Product 4',
+    rating: 3,
+    description: 'Description for Product 4',
+    price: 35
+  },
+  {
+    id: 5,
+    image: '/assets/bike_05.jpg',
+    title: 'Product 5',
+    rating: 3,
+    description: 'Description for Product 5',
+    price: 35
+  },
+  {
+    id: 6,
+    image: '/assets/bike_06.jpg',
+    title: 'Product 6',
+    rating: 3,
+    description: 'Description for Product 6',
+    price: 35
+  },
+  {
+    id: 7,
+    image: '/assets/bike_07.jpg',
+    title: 'Product 7',
+    rating: 3,
+    description: 'Description for Product 7',
+    price: 35
+  },
+  {
+    id: 8,
+    image: '/assets/bike_08.jpg',
+    title: 'Product 8',
+    rating: 3,
+    description: 'Description for Product 8',
+    price: 35
+  }
+]
---
+++ src/json/complements_products.ts
@@ -0,0 +1,90 @@
+export const complements = [
+  {
+    id: 1,
+    image: '/assets/equipment_01.jpg',
+    title: 'Product 1',
+    rating: 4,
+    description: 'Description for Product 1',
+    price: 50
+  },
+  {
+    id: 2,
+    image: '/assets/equipment_02.jpg',
+    title: 'Product 2',
+    rating: 3,
+    description: 'Description for Product 2',
+    price: 35
+  },
+  {
+    id: 3,
+    image: '/assets/equipment_03.jpg',
+    title: 'Product 3',
+    rating: 4,
+    description: 'Description for Product 3',
+    price: 50
+  },
+  {
+    id: 4,
+    image: '/assets/equipment_04.jpg',
+    title: 'Product 4',
+    rating: 3,
+    description: 'Description for Product 4',
+    price: 35
+  },
+  {
+    id: 5,
+    image: '/assets/equipment_05.jpg',
+    title: 'Product 5',
+    rating: 3,
+    description: 'Description for Product 5',
+    price: 35
+  },
+  {
+    id: 6,
+    image: '/assets/equipment_06.jpg',
+    title: 'Product 3',
+    rating: 4,
+    description: 'Description for Product 6',
+    price: 50
+  },
+  {
+    id: 7,
+    image: '/assets/equipment_07.jpg',
+    title: 'Product 7',
+    rating: 3,
+    description: 'Description for Product 7',
+    price: 35
+  },
+  {
+    id: 8,
+    image: '/assets/equipment_08.jpg',
+    title: 'Product 8',
+    rating: 3,
+    description: 'Description for Product 8',
+    price: 35
+  },
+  {
+    id: 9,
+    image: '/assets/equipment_09.jpg',
+    title: 'Product 9',
+    rating: 4,
+    description: 'Description for Product 9',
+    price: 50
+  },
+  {
+    id: 10,
+    image: '/assets/equipment_10.jpg',
+    title: 'Product 10',
+    rating: 3,
+    description: 'Description for Product 10',
+    price: 35
+  },
+  {
+    id: 11,
+    image: '/assets/equipment_11.jpg',
+    title: 'Product 11',
+    rating: 3,
+    description: 'Description for Product 11',
+    price: 35
+  }
+]
