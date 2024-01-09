9cebf825fd3ce89925c2b5c2a5bafb2539c3644e
Gabriel <gszcode@gmail.com>
2024-01-08 23:34:06 -0300

    titulo de pagina agregado

--- src/page/Products/HomeProducts/Sofas/index.tsx
+++ src/page/Products/HomeProducts/Sofas/index.tsx
@@ -2,7 +2,7 @@ import Products from '../..'
 import { sofas } from '../../../../json/home_products'
 
 const Sofas = () => {
-  return <Products products={sofas} page="/ Sofás" />
+  return <Products products={sofas} page="/ Sofás" title="SOFÁS" />
 }
 
 export default Sofas
