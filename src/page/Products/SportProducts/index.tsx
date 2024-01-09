322212a6e6102d52fa7b402e368e14059131206c
Gabriel <gszcode@gmail.com>
2024-01-08 23:33:38 -0300

    pagina de productos deportivos agreegada

--- src/page/Products/SportProducts/index.tsx
+++ src/page/Products/SportProducts/index.tsx
@@ -1,7 +1,14 @@
 import Products from '..'
+import { sports } from '../../../json/sports_products'
 
 const SportProducts = () => {
-  return <Products products={[]} page="/ Productos deportivos" />
+  return (
+    <Products
+      products={sports}
+      page="/ Deportivos"
+      title="PRODUCTOS DEPORTIVOS"
+    />
+  )
 }
 
 export default SportProducts
