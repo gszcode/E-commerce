a083c5095f9c1005dc1575a12573ae05240880bc
Gabriel <gszcode@gmail.com>
2024-01-10 01:22:01 -0300

    se agregaron propiedades de tipos

--- src/typescript/interfaces/product.interface.ts
+++ src/typescript/interfaces/product.interface.ts
@@ -1,9 +1,13 @@
 export interface Product {
   id: number
-  image: string
-  title: string
+  images: Array<string>
+  product_name: string
   rating: number
   description: string
   price: number
   prev_price?: number
+  category: string
+  stock: number
+  characteristics: Array<string>
+  size?: Array<string> | null
 }
