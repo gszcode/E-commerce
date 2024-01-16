07efb8e6f26ea66158f69895996fbac85d4151fc
Gabriel <gszcode@gmail.com>
2024-01-15 23:33:16 -0300

    Componente de carrito

--- src/page/Cart/index.tsx
+++ src/page/Cart/index.tsx
@@ -1,28 +1,59 @@
-import { Link } from 'react-router-dom'
 import AccountTitle from '../../components/AccountTitle'
 import BreadCrumbs from '../../components/BreadCrumbs'
-import Button from '../../components/Button'
 import Wrapper from '../../components/Wrapper'
 import styles from './cart.module.scss'
+import { useEffect, useState } from 'react'
+import EmptyCart from './EmptyCart'
+import Loader from '../../components/Loader'
+import CartContent from './CartContent'
+import { Product } from '../../typescript/interfaces/product.interface'
+import Button from '../../components/Button'
+import Image from '../../components/Image'
+import { TOTAL_PRICE } from '../../utils/getTotalPrice'
+import { storeCart } from '../../utils/getProductsCart'
 
 const Cart = () => {
+  const [cart, setCart] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setLoading(true)
+    setCart(storeCart)
+    setLoading(false)
+  }, [])
+
   return (
     <section className={styles.container}>
       <BreadCrumbs page="Carrito" />
       <Wrapper>
-        <div className={styles.cart}>
-          <AccountTitle title="CARRITO DE COMPRA" />
-          <div className={styles.empty_cart}>
-            <img src="/icons/cart.svg" alt="Cart" />
-            <p className={styles.message}>EL CARRITO ESTÁ VACÍO</p>
-            <span className={styles.description}>
-              No hay artículos en el carrito de compra
-            </span>
-            <Link to="/products">
-              <Button text="CONTINUAR COMPRANDO" />
-            </Link>
+        <>
+          <div className={styles.cart}>
+            <AccountTitle title="CARRITO DE COMPRA" />
+            {loading ? (
+              <Loader />
+            ) : cart.length === 0 ? (
+              <EmptyCart />
+            ) : (
+              cart.map((item: Product) => (
+                <CartContent key={item.id} {...item} />
+              ))
+            )}
           </div>
-        </div>
+          <div className={styles.buy}>
+            <div className={styles.total}>
+              <p className={styles.delete_btn}>
+                TOTAL <small>(Impuestos incluidos)</small>
+              </p>
+              <span className={styles.delete_btn}>{TOTAL_PRICE(cart)}$</span>
+            </div>
+            <div className={styles.action}>
+              <Button text="COMENZAR PEDIDO" />
+              <button className={styles.empty}>
+                <Image img="delete" alt="Delete" /> VACIAR CARRITO
+              </button>
+            </div>
+          </div>
+        </>
       </Wrapper>
     </section>
   )
