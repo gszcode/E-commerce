2f32937dd7e6fbdb2a77b2557304518a63074f2b
Gabriel <gszcode@gmail.com>
2024-01-17 00:47:16 -0300

    se refactorizo el obtener todos los productos

--- src/page/Products/AllProducts/index.tsx
+++ src/page/Products/AllProducts/index.tsx
@@ -1,24 +1,5 @@
 import Products from '..'
-import { products } from '../../../json/offers_products'
-import { blankets, several, sofas } from '../../../json/home_products'
-import {
-  pajamas_blankets,
-  sneakers,
-  underwear
-} from '../../../json/clothes_products'
-import { sports } from '../../../json/sports_products'
-import { bikes } from '../../../json/bikes_products'
-const allProducts = [
-  ...products,
-  ...pajamas_blankets,
-  ...sofas,
-  ...blankets,
-  ...underwear,
-  ...sneakers,
-  ...several,
-  ...sports,
-  ...bikes
-]
+import { allProducts } from '../../../utils/getAllProducts'
 
 const AllProducts = () => {
   return (
