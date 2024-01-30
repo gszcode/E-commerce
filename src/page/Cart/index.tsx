2e8e030bd7ce61fecd822f44615a7fcb8bf79215
Gabriel <gszcode@gmail.com>
2024-01-30 19:59:43 -0300

    se arreglo la orden

--- src/page/Cart/index.tsx
+++ src/page/Cart/index.tsx
@@ -6,23 +6,24 @@ import { useEffect, useState } from 'react'
 import EmptyCart from './EmptyCartOrFavorites'
 import Loader from '../../components/Loader'
 import CartContent from './CartContent'
-import { Product } from '../../typescript/interfaces/product.interface'
 import Button from '../../components/Button'
 import Image from '../../components/Image'
 import { TOTAL_PRICE } from '../../utils/getTotalPrice'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '../../store/store'
 import { setEmptyCart } from '../../store/features/cart/cartSlice'
+import { Order } from '../../typescript/interfaces/order.interface'
 
 const Cart = () => {
   const dispatch = useDispatch()
-  const [cartState, setCartState] = useState<Product[]>([])
   const { cart } = useSelector((state: RootState) => state.cart)
+  const { orders } = useSelector((state: RootState) => state.orders)
   const [loading, setLoading] = useState(true)
+
+  console.log(orders)
 
   useEffect(() => {
     setLoading(true)
-    setCartState(cart)
     setLoading(false)
   }, [cart])
 
@@ -42,7 +43,7 @@ const Cart = () => {
             <EmptyCart page="cart" />
           ) : (
             <>
-              {cartState.map((item: Product) => (
+              {orders.map((item: Order) => (
                 <CartContent key={item.id} {...item} />
               ))}
 
